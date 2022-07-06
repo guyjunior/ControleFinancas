81cea43bc48fb991d155cea02b790782827d6fc6
Guy Junior <guyjunior@iq.ufrj.br>
2022-07-06 15:29:08 -0300

    primeiro

---
+++ financeiro/src/components/Header/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    height: 150px;
+    text-align: center;
+    background: #5f6bf7;
+
+`;
+
+export const Header = styled.h1``;
+
+export const Title = styled.div`
+    padding-top: 20px;
+    color: #fff;
+
+`;
---
+++ financeiro/src/styles/global.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+
+* {
+    margin: 0;
+    padding: 0;
+}
+
+body {
+
+    font-family: 'Poppins', Sans-Serif;
+    background-color: #f4f4f4;
+    }
+`;
+
+export default Global;
